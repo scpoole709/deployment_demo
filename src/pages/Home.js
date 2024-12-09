0fe52b835434bfc40d6b217e31b837637edbf427
Steve Poole <pooledenterprise@aol.com>
2024-12-09 15:39:16 -0500

    try qp

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const Home = () => {
+  
+  const [ searchParams ] = useSearchParams();
+  const navigate = useNavigate();
+
+  useEffect( () => {
+    let qp = searchParams.get("name");
+    if (qp) {
+      navigate("songs/" + qp);
+    }
+  });
+ 
+
   return (
     <div>
       <h1>Welcome</h1>
